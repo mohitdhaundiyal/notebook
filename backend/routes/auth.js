f6584af2524ce67681293980b51803fc32301a33
mohit dhaundiyal <mohit.dhaundiyal22@gmail.com>
2021-10-12 21:43:53 +0530

    Encrypt passwords using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,34 +1,49 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
 const {
     body,
     validationResult
 } = require('express-validator');
 
 router.post('/', [
+    // validations
     body('email', 'Invalid email').isEmail(),
-    body('password', 'Password length must be atleast 6 characters').isLength({
+    body('password', 'Password length must be atleast 6 characters')
+    .isLength({
         min: 6
-    }),
-], (req, res) => {
+    })
+], async (req, res) => {
+
+    // check for validation errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errors: errors.array()
         });
     }
-    User.create({
+
+    // check if email already exists
+    let user = await User.findOne({
+        email: req.body.email
+    });
+
+    if (user) {
+        return (res.status(400).json({
+            error: "Sorry user with this mail already exists"
+        }))
+    }
+
+    // create user
+    const salt = await bcrypt.genSaltSync(10);
+    const cryptPass = await bcrypt.hashSync(req.body.password, salt);
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user)).catch(error => {
-        console.log(error)
-        res.send({
-            error: "This mail is already registered."
-        })
-    });
-    console.log(req.body)
+        password: cryptPass,
+    }).then(user => res.json(user))
+
 })
 
 module.exports = router
