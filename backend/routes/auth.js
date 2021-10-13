9b4e68867cc71fee16ca0699612b896b367313e0
mohit dhaundiyal <mohit.dhaundiyal22@gmail.com>
2021-10-13 22:07:38 +0530

    added login route and authtoken -> jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,8 +6,12 @@ const {
     body,
     validationResult
 } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "bitchhhhh"
 
-router.post('/', [
+
+// Register
+router.post('/register', [
     // validations
     body('email', 'Invalid email').isEmail(),
     body('password', 'Password length must be atleast 6 characters')
@@ -44,6 +48,61 @@ router.post('/', [
         password: cryptPass,
     }).then(user => res.json(user))
 
+
+})
+
+// Authenticate - login
+router.post('/login', [
+    // validations
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Invalid password')
+    .isLength({
+        min: 6
+    })
+], async (req, res) => {
+
+    // check for validation errors 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    // check if email exists or not
+    const {
+        email,
+        password
+    } = req.body;
+
+    try {
+        let user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            res.send('Invalid login, Please use correct credentials.')
+        } else {
+
+            const passCompare = await bcrypt.compare(password, user.password)
+            if (!passCompare) {
+                res.send('Invalid login, Please use correct credentials.')
+            } else {
+                const data = {
+                    user: {
+                        id: user.id
+                    }
+                }
+                const authtoken = jwt.sign(data, JWT_SECRET)
+                res.send({
+                    "authtoken": authtoken
+                })
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("some error occured")
+    }
 })
 
 module.exports = router
